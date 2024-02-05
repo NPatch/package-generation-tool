6f307017de4b9c767cae2d9b286f068c9f598add
Nikolaos Patsiouras <npatsiouras@gmail.com>
2024-02-05 02:02:28 +0200

    Various updates
    * Window now spawns next to SceneView
    * Package Dependency list is populated using EditorCoroutines which
    allows the window to open instantly. Lazy loads the packages.
    * Added Toolbar for the Package Dependencies List with a label and a
    toolbar search button that allows us to search the packages.
    * Switched the Dependency List to a MultiColumnListView which has a
    Toggle per package. Much easier to select than using row multiple
    selection which is easy to invalidate.
    * Added a version override column in the MultiColumnListView that
    allows us to set a custom version for a selected package.

--- Editor/PackageGenerationToolEditor.cs
+++ Editor/PackageGenerationToolEditor.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using Newtonsoft.Json;
@@ -7,7 +8,9 @@ using UnityEditor.PackageManager;
 using UnityEditor.PackageManager.Requests;
 using UnityEngine;
 using UnityEngine.UIElements;
+using Unity.EditorCoroutines.Editor;
 using PackageInfo = UnityEditor.PackageManager.PackageInfo;
+using UnityEditor.UIElements;
 
 namespace PackageGenerationTool.Editor
 {
@@ -164,6 +167,14 @@ namespace PackageGenerationTool.Editor
         public string[] includePlatforms;
     }
 
+    [Serializable]
+    public class PackageInfoProxy
+    {
+        public PackageInfo Info;
+        public bool Selected;
+        public string VersionOverride;
+    }
+
     public class PackageGenerationToolEditor : EditorWindow
     {
         [SerializeField]
@@ -173,7 +184,9 @@ namespace PackageGenerationTool.Editor
 
         private static PackageGenerationToolEditor window;
         private static GUIContent window_title = new GUIContent("Package Generation Tool");
+        private static List<PackageInfoProxy> packages = new List<PackageInfoProxy>();
 
+        private static EditorCoroutine refresh_packages_coro = null;
 
         private TextField package_name_textfield;
         private TextField company_textfield;
@@ -183,7 +196,8 @@ namespace PackageGenerationTool.Editor
         private TextField root_namespace_textfield;
 
         private RadioButtonGroup deployment_type;
-        private ListView dependencies_list;
+        //private ListView dependencies_list;
+        private MultiColumnListView dependencies_list;
 
         private TextField override_package_name_textfield;
         private string override_package_name_cached;
@@ -200,7 +214,7 @@ namespace PackageGenerationTool.Editor
         public static void ShowMyEditor()
         {
             // This method is called when the user selects the menu item in the Editor
-            window = GetWindow<PackageGenerationToolEditor>();
+            window = GetWindow<PackageGenerationToolEditor>(new Type[] { typeof(SceneView) });
             window.titleContent = window_title;
         }
 
@@ -260,52 +274,97 @@ namespace PackageGenerationTool.Editor
                 company_textfield.RegisterCallback<ChangeEvent<string>>(OnPackageNameOrCompanyNameChanged);
             }
 
+            //{
+            //    // The "makeItem" function is called when the
+            //    // ListView needs more items to render.
+            //    Func<VisualElement> makeItem = () =>
+            //    {
+            //        VisualElement row = new VisualElement();
+            //        Label package_label = new Label();
+            //        package_label.name = "package-name";
+            //        package_label.bindingPath = "Info.displayName";
+            //        row.Add(package_label);
+            //        Toggle row_toggle = new Toggle();
+            //        row_toggle.name = "package-selected";
+            //        row_toggle.bindingPath = "Selected";
+            //        row.Add(row_toggle);
+
+            //        row.style.flexShrink = 1;
+            //        row.style.flexGrow = 1;
+            //        return row;
+            //    };
+
+            //    // As the user scrolls through the list, the ListView object
+            //    // recycles elements created by the "makeItem" function,
+            //    // and invoke the "bindItem" callback to associate
+            //    // the element with the matching data item (specified as an index in the list).
+            //    Action<VisualElement, int> bindItem = (e, i) =>
+            //    {
+            //        VisualElement row_root = (e as VisualElement);
+            //        row_root.Q<Label>().text = packages[i].Info.displayName;
+            //        Toggle row_toggle = row_root.Q<Toggle>();
+            //        row_toggle.value = false;
+            //        row_toggle.userData = i;
+            //        row_toggle.RegisterValueChangedCallback<bool>(OnDependencySelected);
+            //    };
+            //    // Provide the list view with an explicit height for every row
+            //    // so it can calculate how many items to actually display
+            //    const int itemHeight = 16;
+
+            //    dependencies_list = root.Q<ListView>("package-dependencies");
+            //    dependencies_list.itemsSource = packages;
+            //    dependencies_list.fixedItemHeight = itemHeight;
+            //    dependencies_list.makeItem = makeItem;
+            //    dependencies_list.bindItem = bindItem;
+            //    dependencies_list.selectionType = SelectionType.Multiple;
+            //    dependencies_list.RefreshItems();
+            //}
+
             {
-                var items = new List<string>();
+                dependencies_list = root.Q<MultiColumnListView>();
+                // Call MultiColumnTreeView.SetRootItems() to populate the data in the tree.
+                dependencies_list.itemsSource = packages;
+
+                // For each column, set Column.makeCell to initialize each node in the tree.
+                // You can index the columns array with names or numerical indices.
+                dependencies_list.columns["selected"].makeCell = () => new Toggle();
+                dependencies_list.columns["package-name"].makeCell = () => new Label();
+                dependencies_list.columns["package-version-override"].makeCell = () => new TextField();
+
+                // For each column, set Column.bindCell to bind an initialized node to a data item.
+                dependencies_list.columns["selected"].bindCell = (VisualElement element, int index) =>
                 {
-                    ListRequest package_list_request = Client.List(false);
-                    while (!package_list_request.IsCompleted) { }
-
-                    PackageCollection built_in = package_list_request.Result;
-
-                    foreach (PackageInfo pkg_info in built_in)
-                    {
-                        items.Add(pkg_info.name);
-                    }
-                }
-
+                    Toggle t = (element as Toggle);
+                    t.value = packages[index].Selected;
+                    t.userData = index;
+                    t.RegisterValueChangedCallback<bool>(OnDependencySelected);
+                };
+                dependencies_list.columns["package-name"].bindCell = (VisualElement element, int index) =>
+                    (element as Label).text = packages[index].Info.displayName;
+                dependencies_list.columns["package-version-override"].bindCell = (VisualElement element, int index) =>
                 {
-                    SearchRequest package_list_request = Client.SearchAll(false);
-                    while (!package_list_request.IsCompleted) { }
+                    TextField t = (element as TextField);
+                    t.SetValueWithoutNotify(packages[index].VersionOverride);
+                    t.userData = index;
+                    t.RegisterValueChangedCallback<string>(OnDependencyVersionOverriden);
+                };
 
-                    PackageInfo[] remote_packages = package_list_request.Result;
-
-                    foreach (PackageInfo pkg_info in remote_packages)
+                ToolbarSearchField dep_search = root.Q<ToolbarSearchField>();
+                dep_search.Q<Button>().clicked += ()=> {
+                    if (!string.IsNullOrEmpty(dep_search.value))
                     {
-                        items.Add(pkg_info.name);
+                        for(int i = 0; i < packages.Count; i++)
+                        {
+                            if (packages[i].Info.displayName.ToLower().Contains(dep_search.value.ToLower()))
+                            {
+                                dependencies_list.ScrollToItem(i);
+                                dependencies_list.selectedIndex = i;
+                                break;
+                            }
+                        }
                     }
-                }
-
-                // The "makeItem" function is called when the
-                // ListView needs more items to render.
-                Func<VisualElement> makeItem = () => new Label();
-
-                // As the user scrolls through the list, the ListView object
-                // recycles elements created by the "makeItem" function,
-                // and invoke the "bindItem" callback to associate
-                // the element with the matching data item (specified as an index in the list).
-                Action<VisualElement, int> bindItem = (e, i) => (e as Label).text = items[i];
-
-                // Provide the list view with an explicit height for every row
-                // so it can calculate how many items to actually display
-                const int itemHeight = 16;
-
-                dependencies_list = root.Q<ListView>("package-dependencies");
-                dependencies_list.itemsSource = items;
-                dependencies_list.fixedItemHeight = itemHeight;
-                dependencies_list.makeItem = makeItem;
-                dependencies_list.bindItem = bindItem;
-                dependencies_list.selectionType = SelectionType.Multiple;
+                };
+                dep_search.RegisterValueChangedCallback(evt => Debug.Log("New search value: " + evt.newValue));
             }
 
             {//RefreshPackages
@@ -315,28 +374,70 @@ namespace PackageGenerationTool.Editor
             {//Generate
                 generate_package_btn.clicked += OnGenerateClicked;
             }
+
+
+            if(refresh_packages_coro != null)
+            {
+                EditorCoroutineUtility.StopCoroutine(refresh_packages_coro);
+                refresh_packages_coro = null;
+            }
+            refresh_packages_coro = EditorCoroutineUtility.StartCoroutine(RefreshPackageList(), this);
+            //Highlighter.Highlight(window.name, "tool-panel");
+        }
+
+        IEnumerator RefreshPackageList(bool only_remote = true)
+        {
+            if(!only_remote)
+            {
+                ListRequest package_list_request = Client.List(false);
+                while (!package_list_request.IsCompleted)
+                {
+                    yield return new EditorWaitForSeconds(1.0f);
+                }
+                
+                PackageCollection built_in = package_list_request.Result;
+
+                foreach (PackageInfo pkg_info in built_in)
+                {
+                    packages.Add(new PackageInfoProxy()
+                    {
+                        Info = pkg_info,
+                        Selected = false,
+                        VersionOverride = pkg_info.version
+                    });
+                }
+            }
+
+            {
+                SearchRequest package_list_request = Client.SearchAll(false);
+                while (!package_list_request.IsCompleted)
+                {
+                    yield return new EditorWaitForSeconds(1.0f);
+                }
+                PackageInfo[] remote_packages = package_list_request.Result;
+
+                foreach (PackageInfo pkg_info in remote_packages)
+                {
+                    packages.Add(new PackageInfoProxy()
+                    {
+                        Info = pkg_info,
+                        Selected = false,
+                        VersionOverride = pkg_info.version
+                    });
+                }
+            }
+
+            dependencies_list.RefreshItems();
         }
 
         private void OnRefreshPackagesClicked()
         {
-            SearchRequest package_list_request = Client.SearchAll(false);
-            while (!package_list_request.IsCompleted) { }
-
-            PackageInfo[] remote_packages = package_list_request.Result;
-
-            IList<string> deps = dependencies_list.itemsSource as IList<string>;
-
-            foreach (PackageInfo pi in remote_packages)
+            if (refresh_packages_coro != null)
             {
-                if (deps.Contains(pi.name))
-                {
-                    continue;
-                }
-                else
-                {
-                    dependencies_list.itemsSource.Add(pi.name);
-                }
+                EditorCoroutineUtility.StopCoroutine(refresh_packages_coro);
+                refresh_packages_coro = null;
             }
+            refresh_packages_coro = EditorCoroutineUtility.StartCoroutine(RefreshPackageList(), this);
         }
 
         private void OnPackageNameOrCompanyNameChanged(ChangeEvent<string> evt)
@@ -398,6 +499,20 @@ namespace PackageGenerationTool.Editor
             return fully_qualified_package_name.ToLower();
         }
 
+        private void OnDependencySelected(ChangeEvent<bool> evt)
+        {
+            Toggle t = (Toggle)evt.target;
+            PackageInfoProxy pip = packages[(int)t.userData];
+            pip.Selected = evt.newValue;
+        }
+
+        private void OnDependencyVersionOverriden(ChangeEvent<string> evt)
+        {
+            Toggle t = (Toggle)evt.target;
+            PackageInfoProxy pip = packages[(int)t.userData];
+            pip.VersionOverride = evt.newValue;
+        }
+
         private string RegeneratePackageDisplayName()
         {
             string package_display_name = string.Format($"{company_textfield.value}.{package_name_textfield.value}");
@@ -417,14 +532,6 @@ namespace PackageGenerationTool.Editor
 
             DeploymentType deploy = (DeploymentType)deployment_type.value;
 
-            List<string> dependency_pkg_list = new List<string>();
-            IList<string> deps = dependencies_list.itemsSource as IList<string>;
-            foreach (int pkg_index in dependencies_list.selectedIndices)
-            {
-                dependency_pkg_list.Add(deps[pkg_index]);
-            }
-            IEnumerable<object> selected_pkgs = dependencies_list.selectedItems;
-
             SerializablePackageInfo spi = new SerializablePackageInfo()
             {
                 Name = fully_qualified_name,
@@ -433,10 +540,15 @@ namespace PackageGenerationTool.Editor
                 Description = package_description,
                 Author = author,
                 Unity = unity_min_version,
-                Dependencies = GeneratePackageDependencyList(dependency_pkg_list)
+                Dependencies = GeneratePackageDependencyList()
             };
 
+            foreach(SerializableDependencyInfo sdi in spi.Dependencies)
+            {
+                Debug.Log($"{sdi.name}: {sdi.version}");
+            }
 
+            return;
             DirectoryInfo project_root_di = new DirectoryInfo(Application.dataPath).Parent;
             DirectoryInfo package_root_di = null;
             {//Create the package folder first
@@ -505,19 +617,16 @@ namespace PackageGenerationTool.Editor
             }
         }
 
-        private List<SerializableDependencyInfo> GeneratePackageDependencyList(List<string> package_list)
+        private List<SerializableDependencyInfo> GeneratePackageDependencyList()
         {
             List<SerializableDependencyInfo> package_dependencies = new List<SerializableDependencyInfo>();
-            foreach(string package_name in package_list)
+            foreach(PackageInfoProxy pip in packages)
             {
-                SearchRequest search_req = Client.Search(package_name);
-                while (!search_req.IsCompleted)
-                {}
-
+                if (!pip.Selected) continue;
                 SerializableDependencyInfo dependency = new SerializableDependencyInfo()
                 {
-                    name = package_name,
-                    version = search_req.Result[0].version
+                    name = pip.Info.name,
+                    version = pip.Info.version
                 };
 
                 package_dependencies.Add(dependency);
